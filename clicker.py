ba2161c6067daa0ca2d1c266abe0b8db2530d598
ngaziz <ngaziz83@gmail.com>
2019-08-07 20:45:31 +0500

    Added game menu

--- clicker.py
+++ clicker.py
@@ -1,7 +1,27 @@
 import pygame
 from random import randrange
 
-# get random color except black 
+# Init game
+pygame.init()
+screen_width = 640
+screen_height = 480
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Clicker')
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (30, 30, 30)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+# Game Framerate
+clock = pygame.time.Clock()
+FPS = 30
+
+# Get random color except black
 def get_color():
     red = randrange(255)
     green = randrange(255)
@@ -12,23 +32,62 @@ def get_color():
         color = (red, green, blue)
     return color
 
+# Text Renderer
+def text_format(message, textSize, textColor):
+    newFont = pygame.font.SysFont("arial", textSize)
+    newText = newFont.render(message, 0, textColor)
+    return newText
+
+# Main Menu
+def main_menu():
+    menu=True
+    selected="start"
+    while menu:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_UP:
+                    selected="start"
+                elif event.key==pygame.K_DOWN:
+                    selected="quit"
+                if event.key==pygame.K_RETURN:
+                    if selected=="start":
+                        main()
+                    if selected=="quit":
+                        pygame.quit()
+                        quit()
+        # Main Menu UI
+        screen.fill(black)
+        title=text_format("Clicker", 90, yellow)
+        if selected=="start":
+            text_start=text_format("START", 75, white)
+        else:
+            text_start = text_format("START", 75, gray)
+        if selected=="quit":
+            text_quit=text_format("QUIT", 75, white)
+        else:
+            text_quit = text_format("QUIT", 75, gray)
+
+        title_rect=title.get_rect()
+        start_rect=text_start.get_rect()
+        quit_rect=text_quit.get_rect()
+
+        # Main Menu Text
+        screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
+        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
+        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 360))
+        pygame.display.update()
+        clock.tick(FPS)
+
 def main():
-    # init game
-    pygame.init()
-    clock = pygame.time.Clock()
-    screen_width = 640
-    screen_height = 480
-    icon = pygame.image.load('icon.png')
-    pygame.display.set_icon(icon)
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption('Clicker')
     font = pygame.font.SysFont("arial", 16)
-    
     level = 1
     score = 0
     hits = 0
     miss = 0
-    
+
     # init ball
     color = get_color()
     radius = 0
@@ -36,7 +95,6 @@ def main():
     pos = (10+randrange(screen_width-10), 10+randrange(screen_height-10))
     
     run = True
-
     # main game cycle    
     while run:
         # process events
@@ -81,7 +139,7 @@ def main():
         if score <= -100:
             run = False
         # delay
-        clock.tick(60)
+        clock.tick(FPS)
         
     # game over
     screen.fill((0, 0, 0))
@@ -92,4 +150,4 @@ def main():
     pygame.event.clear()
     pygame.event.wait()
 
-main()
+main_menu()
